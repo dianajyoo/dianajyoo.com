7b179043335a1d865d35c0ed34cd2ac6245a9302
Diana Yoo <diana@thesistesting.com>
2020-12-26 14:10:10 -0500

    remove filter in safari

--- src/js/index.js
+++ src/js/index.js
@@ -22,6 +22,13 @@ function enlargeImage(className) {
   img.style.cursor = 'default';
 }
 
+function removeFilter() {
+  if (isSafari()) {
+    const gradient = document.querySelector('.radial-gradient');
+    gradient.style.filter = 'none';
+  }
+}
+
 function grow() {
   const bars = document.querySelectorAll('.bar');
   bars.forEach((bar) => bar.classList.add('is-grow'));
@@ -31,7 +38,13 @@ function isDesktop() {
   return window.innerWidth >= 1024;
 }
 
+function isSafari() {
+  return navigator.vendor.indexOf('Apple') > -1;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+  removeFilter();
+
   document.addEventListener('click', (e) => {
     if (e.target.tagName === 'A') {
       showDiv(e.target);
