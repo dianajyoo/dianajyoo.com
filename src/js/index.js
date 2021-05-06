86dcd3ec1820d0a044e5f6727529f9cb79fbd3d5
Diana Yoo <diana@thesistesting.com>
2021-05-05 22:42:00 -0400

    fix scss

--- src/js/index.js
+++ src/js/index.js
@@ -7,44 +7,17 @@ function toggleView(el) {
   }
 
   hiddenDiv.classList.remove('is-hidden');
-  this.currentView = hiddenDiv
+  this.currentView = hiddenDiv;
 
   if (hiddenDiv.id === 'whoami') {
     grow();
   }
 }
 
-// function enlargeImage(className) {
-//   const img = document.querySelector(`.${className}`);
-
-//   if (!img.classList.contains('is-modal-open')) {
-//     img.classList.add('is-modal-open');
-//     img.style.objectFit = 'contain';
-//     img.style.cursor = 'zoom-out';
-//     return;
-//   }
-//   img.classList.remove('is-modal-open');
-//   img.style.objectFit = 'cover';
-//   img.style.cursor = 'default';
-// }
-
-function grow() {
-  const bars = document.querySelectorAll('.bar');
-  bars.forEach((bar) => bar.classList.add('is-grow'));
-}
-
-function isDesktop() {
-  return window.innerWidth >= 1024;
-}
-
-function isSafari() {
-  return navigator.vendor.indexOf('Apple') > -1;
-}
-
 function init() {
   this.currentView = document.querySelector('#projects');
 
-  document.addEventListener('click', (e) => {
+  document.addEventListener('click', e => {
     if (e.target.tagName === 'A') {
       toggleView(e.target);
     }
